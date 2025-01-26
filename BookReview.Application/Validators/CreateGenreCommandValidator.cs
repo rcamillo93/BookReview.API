6b9a89bc39cd006109cbfbf104d57bd9dc58b39a
rcamillo93 <rafael.camillo14@gmail.com>
2025-01-26 15:32:05 -0300

    Validação dos dados de entrada no gênero do livro

---
+++ BookReview.Application/Validators/CreateGenreCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using BookReview.Application.Commads.GenreCommands.Create;
+using FluentValidation;
+
+namespace BookReview.Application.Validators
+{
+    public class CreateGenreCommandValidator : AbstractValidator<CreateGenreCommand>
+    {
+        public CreateGenreCommandValidator()
+        {
+            RuleFor(g => g.Description)
+                  .NotEmpty()
+                    .WithMessage("A descrição é obrigatória");
+        }
+    }
+}
